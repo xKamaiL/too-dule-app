4f4ba9648bd8e86b520caec606529f2bb2a7fbed
xKamaiL <xkamail@icloud.com>
2021-08-24 07:31:54 +0700

    fix: json only post put

--- pkg/middlewares/json_body.go
+++ pkg/middlewares/json_body.go
@@ -8,13 +8,16 @@ import (
 
 func NeededJSONBody(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if !strings.Contains(r.Header.Get("Content-Type"), "application/json") {
-			msg := "Content-Type header is not application/json"
-			_ = utils.JSONError(w, msg, http.StatusUnsupportedMediaType)
-			return
+		if r.Method == http.MethodPut || r.Method == http.MethodPost {
+			if !strings.Contains(r.Header.Get("Content-Type"), "application/json") {
+				msg := "Content-Type header is not application/json"
+				_ = utils.JSONError(w, msg, http.StatusUnsupportedMediaType)
+				return
+			}
+			// Max size of json to 1MB
+			r.Body = http.MaxBytesReader(w, r.Body, 1048576)
+
 		}
-		// Max size of json to 1MB
-		r.Body = http.MaxBytesReader(w, r.Body, 1048576)
 
 		next.ServeHTTP(w, r)
 	})
